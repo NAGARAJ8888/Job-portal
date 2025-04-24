69ce8c87f83dee3db0896bdfbbdb6356c9e1278a
NAGARAJ8888 <nagaraj81471@gmail.com>
2025-04-24 10:08:14 +0530

    Initial and final commit

---
+++ frontend/src/components/Auth/Login.jsx
@@ -0,0 +1,100 @@
+import { useContext, useState } from "react";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLock2Fill } from "react-icons/ri";
+import { Link, Navigate } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Context } from "../../main";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+
+  const { isAuthorized, setIsAuthorized } = useContext(Context);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/user/login",
+        { email, password, role },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message);
+      setEmail("");
+      setPassword("");
+      setRole("");
+      setIsAuthorized(true);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if(isAuthorized){
+    return <Navigate to={'/'}/>
+  }
+
+  return (
+    <>
+      <section className="authPage">
+        <div className="container">
+          <div className="header">
+            <img src="/daily-jobs-logo.png" alt="logo" />
+            <h3>Login to your account</h3>
+          </div>
+          <form>
+            <div className="inputTag">
+              <div>
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="">Login as</option>
+                  
+                  <option value="Job Seeker">Job Seeker</option>
+                  <option value="Employer">Employer</option>
+                </select>
+                <FaRegUser />
+              </div>
+            </div>
+            <div className="inputTag">
+              <div>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <MdOutlineMailOutline />
+              </div>
+            </div>
+            <div className="inputTag">
+              <div>
+                <input
+                  type="password"
+                  placeholder="Enter your Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <RiLock2Fill />
+              </div>
+            </div>
+            <button type="submit" onClick={handleLogin}>
+              Login
+            </button>
+            <span>Don’t have an account? <Link to={"/register"}> Register Now</Link></span>
+          </form>
+        </div>
+        <div className="banner">
+          <img src="/login-side-image.jpg" alt="login" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
