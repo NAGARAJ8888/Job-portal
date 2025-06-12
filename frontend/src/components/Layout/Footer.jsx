87f3c6275e9115919c51d1c48d9bf438cddc6342
NAGARAJ8888 <105354698+NAGARAJ8888@users.noreply.github.com>
2025-06-12 13:47:13 +0530

    Update Footer.jsx

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -12,10 +12,10 @@ function Footer() {
     <div>&copy; All Rights Reserved by Daily Jobs.</div>
     
     <div>
-      <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
-      <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-      <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-      <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+      <Link to={'https://github.com/'} target='github'><FaGithub></FaGithub></Link>
+      <Link to={'https://leetcode.com/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
+      <Link to={'https://www.linkedin.com/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
+      <Link to={'https://www.instagram.com/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
     </div>
       
     </footer>
